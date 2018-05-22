9f662510fc168c935e400d5ee6377006562b18d7
Kennan LeJeune <kennan@case.edu>
2018-05-22 02:40:13 -0500

    removed sudo from within script

--- configureScript.sh
+++ configureScript.sh
@@ -3,9 +3,6 @@ if [ ! -e /usr/local/bin/brew ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# clone the repository
-git clone https://github.com/kclejeune/RegistrationBot.git
-
 # install necessary dependencies
 brew cask install chromedriver
 brew install python
@@ -13,5 +10,9 @@ brew install python
 # navigate to the directory and install requirements
 cd RegistrationBot
 pip3 install -r requirements.txt
+
+# change time server to match SIS
+/usr/sbin/systemsetup -setnetworktimeserver "tick.usno.navy.mil"
+/usr/sbin/systemsetup -setusingnetworktime on
 clear
 echo "Installation complete. Refer to README.md for running instructions."
