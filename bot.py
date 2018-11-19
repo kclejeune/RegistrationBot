5b378ea7fa93ba439c4e2949da640e7cc3803f42
Kennan LeJeune <kennanlejeune@Randy.local>
2018-11-19 16:39:19 -0500

    general refactoring

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 from sys import argv
 import time
 from datetime import datetime
-
+from datetime import timedelta
 import pause
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -13,31 +13,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-# prompt for login credentials
-if len(argv) >= 4:
-    usernameStr = argv[1]
-    passwordStr = argv[2]
-    semester = argv[3]
-else:
-    # prompt for username
-    usernameStr = input("SIS Username: ")
-    # prompt for password using getpass to ensure password security
-    passwordStr = getpass.getpass("SIS Password: ")
-    # prompt for the semester being registered for to determine the cart link code
-    semester = input(
-        "Semester (f = fall, s = spring, or paste SIS Mobile Cart Link): ")
+test = len(argv) >= 2 and (argv[1] == '-t' or argv[1] == '--test')
 
-# optional - allows for headless browser configuration, theoretically faster script
-chrome_options = Options()
-# uncomment for headless browser functionality
-if len(argv) >= 5:
-    if argv[4] == '-h':
-        chrome_options.add_argument("--headless")
-        print("Running Headless...")
+# prompt for username
+usernameStr = input("SIS Username: ")
+# prompt for password using getpass to ensure password security
+passwordStr = getpass.getpass("SIS Password: ")
+# prompt for the semester being registered for to determine the cart link code
+semester = input(
+    "Semester (f = fall, s = spring, or paste SIS Mobile Cart Link): ")
 
-# get current month
 month = datetime.now().month
-
 # if it's the day of registration
 if datetime.now().time().hour < 7:
     day = datetime.now().day
@@ -65,36 +51,37 @@ if semester == 'f':
 elif semester == 's':
     # similar sketchy math
     cart_link += str(math.floor(datetime.now().year / 1000))
-    cart_link += str(datetime.now().year + 1 -
-                     math.floor(datetime.now().year / 1000) * 1000)
+    cart_link += str(datetime.now().year + 1 - math.floor(datetime.now().year / 1000) * 1000)
     # signifies january as first month of classes
     cart_link += '1'
 else:
     # copy the link
     cart_link = semester
 
-# begin two minutes before registration is supposed to open
-start_time = datetime(datetime.now().year, month, day, 6, 58)
+if test:
+    print("Testing script...")
+    start_time = datetime.now()
+else:
+    # begin two minutes before registration is supposed to open
+    start_time = datetime(datetime.now().year, month, day, 6, 58)
 
-pause.seconds(.01)
-pause.until(start_time)
 print("Script will begin at", start_time, "AM")
-
-
-driver = webdriver.Chrome(chrome_options=chrome_options,
-                          executable_path="/usr/local/bin/chromedriver")
+pause.until(start_time)
+driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 driver.get(cart_link)
 
 # wait for the login screen to load
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, 'login-btn')))
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login-btn')))
 
+# enter username
 username = driver.find_element_by_name("username")
 username.send_keys(usernameStr)
 
+# enter password
 password = driver.find_element_by_name('password')
 password.send_keys(passwordStr)
 
+# click login
 login = driver.find_element_by_id('login-btn')
 login.click()
 
@@ -102,14 +89,20 @@ login.click()
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, 'cart-select-all')))
 
-selectAll = driver.find_element_by_id('cart-select-all')
-selectAll.click()
-
+select_all = driver.find_element_by_id('cart-select-all')
+select_all.click()
 enroll = driver.find_element_by_id('enroll')
 
-# wait until 7:00AM and then click
-pause.until(datetime(datetime.now().year, month, day, 7))
+# wait until 7:00AM and then click enroll
+if not test:
+    pause.until(datetime(datetime.now().year, month, day, 7))
+else:
+    pause.until(datetime(datetime.now().year, datetime.now().month, datetime.now().day, 14, 56))
+
 enroll.click()
 WebDriverWait(driver, 10)
 
-print("Successfully Registered")
+if EC.presence_of_element_located((By.ID, 'cart-select-all')):
+    print("Registration Failed")
+else:
+    print("Successfully Registered")
