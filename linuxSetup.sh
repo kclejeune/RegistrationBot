18f91771d25e2f321a093c7336b917e3e1dd362b
Kennan LeJeune <kclejeune@Randy.local>
2019-04-15 23:09:09 -0400

    update install scripts

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# clone the repository
-git clone https://github.com/kclejeune/RegistrationBot.git
-
-# install necessary dependencies
+# Install necessary dependencies
 sudo apt install chromium-chromedriver python3 python3-pip
-clear
-
-# navigate to the directory and install requirements
-cd ~/RegistrationBot
-chmod +x bot.py
 pip3 install -r requirements.txt
 clear
 
-echo "Installation complete. Refer to README.md for running instructions."
+echo "Dependency installation complete. Refer to README.md for running instructions."
--- macSetup.sh
+++ macSetup.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-# clone the repository
-git clone https://github.com/kclejeune/RegistrationBot.git
-
 # check if homebrew is installed and install if not
 if [ ! -e /usr/local/bin/brew ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # install necessary dependencies
-brew cask install chromedriver
+brew cask install google-chrome chromedriver
 brew install python
-clear
-
-# navigate to the directory and install requirements
-cd ~/RegistrationBot
 pip3 install -r requirements.txt
-clear
 
 # change time server to match SIS
 sudo /usr/sbin/systemsetup -setnetworktimeserver "tick.usno.navy.mil"
