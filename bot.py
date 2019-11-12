e113816a49093b1ffb8cf92f4ca42ebeddec4a7c
Imran Hossain <imran3740@gmail.com>
2019-11-12 20:25:45 +0000

    Update for new SIS

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 import getpass
 import math
-import os
-from sys import argv
-import time
 from datetime import datetime
 from datetime import timedelta
+from sys import argv
+
 import pause
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -32,12 +30,13 @@ if today.hour > 7:
     enroll_date += timedelta(days=1)
 
 # base cart link for sis-mobile
-cart_link = "https://sismobile.case.edu/app/student/enrollmentcart/cart/CASE1/UGRD/"
+# cart_link = "https://sisadmin.case.edu/psc/P92SCWR_18/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV&ICAJAXTrf=true"
+cart_link = "https://sisadmin.case.edu/psp/P92SCWR/?cmd=login"
 
 # sketchy math to compute cart link code and append to base link
 month = datetime.now().month
 # if we're in the fall semester, register for the spring
-if month >= 8 and month <= 12:
+if 8 <= month <= 12:
     # similar sketchy math
     cart_link += str(math.floor(datetime.now().year / 1000))
     cart_link += str(datetime.now().year + 1 - math.floor(datetime.now().year / 1000) * 1000)
@@ -62,32 +61,44 @@ else:
 
 print("Script will begin at", start_time)
 pause.until(start_time)
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.get(cart_link)
 
 # wait for the login screen to load
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login-btn')))
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login')))
 
 # enter username
-username = driver.find_element_by_name("username")
+username = driver.find_element_by_name("userid")
 username.send_keys(usernameStr)
 
 # enter password
-password = driver.find_element_by_name('password')
+password = driver.find_element_by_name('pwd')
 password.send_keys(passwordStr)
 
 # click login
-login = driver.find_element_by_id('login-btn')
+login = driver.find_element_by_name('Sign in')
 login.click()
 
-# wait for cart to load
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, 'cart-select-all')))
+driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Classes & Enrollment'])[1]/following::div[3]").click()
+pause.seconds(5)
+driver.find_element_by_link_text("Shopping Cart").click()
+pause.seconds(3)
+driver.find_element_by_link_text("Spring 2020").click()
 
-# find the necessary buttons
-select_all = driver.find_element_by_id('cart-select-all')
-select_all.click()
-enroll = driver.find_element_by_id('enroll')
+# # wait for cart to load
+# WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.ID, "win15divACAD_CAR_TBL_DESCR$1")))
+#
+# # Select term
+# driver.find_element_by_id("win15divACAD_CAR_TBL_DESCR$1").click()
+
+pause.seconds(3)
+# Select courses
+chkboxes = driver.find_elements_by_class_name("ps-checkbox")
+for c in chkboxes:
+    c.click()
+
+driver.find_element_by_link_text("Enroll").click()
 
 # click enroll in 5 seconds from now if testing
 if test:
@@ -96,17 +107,7 @@ if test:
 # pause until 7AM and click immediately after
 print("Enrolling at: ", enroll_date)
 pause.until(enroll_date)
-enroll.click()
 
-# keep clicking until a minute after in case we miss it
-while datetime.now() < enroll_date + timedelta(minutes=1):
-    # once the cart page loads again, find the buttons again and click them
-    if EC.presence_of_element_located((By.ID, 'cart-select-all')):
-        driver.find_element_by_id('cart-select-all').click()
-        driver.find_element_by_id('enroll').click()
+driver.find_element_by_link_text("Yes").click()
 
-# if we're still on the cart page, we messed up :(
-if EC.presence_of_element_located((By.ID, 'cart-select-all')):
-    print("Registration Failed")
-else:
-    print("Registration Successful")
+print("uhhhhh i guess im done here")
